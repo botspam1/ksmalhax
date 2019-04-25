9e12a62d7aa494b1559903521d385f57fb712708
botspam1 <49961396+botspam1@users.noreply.github.com>
2019-04-25 02:19:26 -1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "+";
 var adminprefix = '#'
 
 
-const developers = ["ID","ID"]
+const developers = ["570963100002680842"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
